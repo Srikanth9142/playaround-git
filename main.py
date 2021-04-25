93f459b4cfbfb4a9e8f14c50bb755f90314da811
srikanth9142 <srikanthtumpudi1999@gmail.com>
2021-04-25 10:21:24 +0530

    Add calculator class

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from helper.add import add
 from helper.subtract import sub
 
-def main():
-	a = 10
-	b = 5
-	print(add(a,b))
-	print(sub(a,b))
-main()
+class Calculator:
+	def add_method(self,a,b):
+		print("Result is "+str(add(a,b)))
+
+	def sub_method(self,a,b):
+		print("Result is "+str(sub(a,b)))
+
+calc = Calculator()
+calc.add_method(10,2)
+calc.sub_method(10,2)
+
